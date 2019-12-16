ece71bd8267c471a966f94e601789762207fb8bc
Cody Garrett <cody.l.garrett22@gmail.com>
2019-12-16 13:29:21 -0700

    project complete?

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,1 +1,21 @@
 // Test away!
+import React from "react";
+import { render, fireEvent } from "@testing-library/react";
+
+import Controls from "./Controls";
+
+test("If gate is locked, show 'unlock gate'. Otherwise show 'lock gate'", () => {
+    //button shows 'unlock gate' as text
+    const {getByText, rerender} = render(<Controls locked={true}/>);
+    getByText(/unlock gate/i);
+
+    rerender(<Controls locked={false}/>);
+    getByText(/lock gate/i);
+});
+
+test("When clicking 'unlock gate', text changes to 'lock gate'", () => {
+    const { getByText } = render(<Controls locked={true}/>);
+    const button = getByText(/unlock gate/i);
+    fireEvent.click(button);
+    getByText(/lock gate/i);
+})
--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,21 @@
 // Test away!
+import React from "react";
+import { render } from "@testing-library/react";
+
+import Display from "./Display";
+
+test("Displays correct text based on lock status", () => {
+    const { getByText, rerender } = render(<Display locked={true} />);
+    getByText(/locked/i);
+
+    rerender(<Display locked={false}/>);
+    getByText(/unlock/i);
+})
+
+test("Displays correct text based on open status", () => {
+    const { getByText, rerender } = render(<Display closed={true}/>);
+    getByText(/closed/i);
+
+    rerender(<Display closed={false} />);
+    getByText(/open/i);
+})
